fdc19887d6159b7205be20fb1066728bc369ed62
overfloat <rillrate@gmail.com>
2020-12-29 22:20:55 +0300

    Add meta macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+meta!();
+
+#[macro_export]
+macro_rules! meta {
+    () => {
+        pub mod meta {
+            pub static VERSION: &str = env!("CARGO_PKG_VERSION");
+            pub static AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
+            pub static NAME: &str = env!("CARGO_PKG_NAME");
+            pub static DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+        }
+    };
 }
