b433fefe25bbd0dcd758eba30d17381a3dfb0903
overfloat <rillrate@gmail.com>
2021-01-08 12:27:27 +0300

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,15 @@ meta!();
 #[macro_export]
 macro_rules! meta {
     () => {
+        /// The metadata of the crate.
         pub mod meta {
+            /// Version.
             pub static VERSION: &str = env!("CARGO_PKG_VERSION");
+            /// List of authors.
             pub static AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
+            /// Name.
             pub static NAME: &str = env!("CARGO_PKG_NAME");
+            /// Description.
             pub static DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
         }
     };
